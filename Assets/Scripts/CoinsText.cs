1e319162d64cc3379bbe6f3b31848b77d9f5fa94
Leonardo <leo.sopranzetti@hotmail.com>
2023-11-30 12:41:46 -0300

    player upgrades

---
+++ Assets/Scripts/CoinsText.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CoinsText : MonoBehaviour
+{
+    [SerializeField] DataContainer dataContainer;
+    TMPro.TextMeshProUGUI text;
+
+    private void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "Coins" + dataContainer.coins.ToString();
+    }
+}
---
+++ Assets/Scripts/PlayerUpgradeUIElement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PlayerUpgradeUIElement : MonoBehaviour
+{
+    [SerializeField] PlayerPersistentUpgrades upgrade;
+    [SerializeField] TextMeshProUGUI upgradeName;
+    [SerializeField] TextMeshProUGUI level;
+    [SerializeField] TextMeshProUGUI price;
+    [SerializeField] DataContainer dataContainer;
+
+    private void Start()
+    {
+        UpdateEelement();
+    }
+
+    public void Upgrade()
+    {
+        PlayerUpgrades playerUpgrades = dataContainer.upgrades[(int)upgrade];
+
+        if (dataContainer.coins >= playerUpgrades.costToUpgrade)
+        {
+            dataContainer.coins -= playerUpgrades.costToUpgrade;
+            playerUpgrades.level += 1;
+            UpdateEelement();
+        }
+    }
+
+    void UpdateEelement()
+    {
+        PlayerUpgrades playerUpgrade = dataContainer.upgrades[(int)upgrade];
+        upgradeName.text = upgrade.ToString();
+        level.text = playerUpgrade.level.ToString();
+        price.text = playerUpgrade.costToUpgrade.ToString();
+
+    }
+
+}
